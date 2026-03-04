dfc84053f735cbda91ffc5c8bf7be77b70dde384
Matthew Hrehirchuk <me@matthew-hre.com>
2026-03-03 20:16:39 -0700

    ci: lint

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,11 @@
-import { createClient } from '@supabase/supabase-js';
-import type { Database } from '../types/database';
+import { createClient } from "@supabase/supabase-js";
+
+import type { Database } from "../types/database";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
-export default supabase
+export default supabase;
         
--- src/types/database.ts
+++ src/types/database.ts
@@ -1,17 +1,26 @@
-export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json | undefined }
-  | Json[]
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof DefaultSchema["CompositeTypes"]
+    | { schema: keyof DatabaseWithoutInternals },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
+    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never;
 
 export type Database = {
   graphql_public: {
-    Tables: {
+    CompositeTypes: {
       [_ in never]: never
     }
-    Views: {
+    Enums: {
       [_ in never]: never
     }
     Functions: {
@@ -25,273 +34,190 @@ export type Database = {
         Returns: Json
       }
     }
-    Enums: {
+    Tables: {
       [_ in never]: never
     }
-    CompositeTypes: {
+    Views: {
       [_ in never]: never
     }
   }
   public: {
-    Tables: {
-      artists: {
-        Row: {
-          artist_id: number
-          artist_image_url: string | null
-          artist_name: string | null
-          artist_type_id: number | null
-          spotify_desc: string | null
-          spotify_url: string | null
-        }
-        Insert: {
-          artist_id: number
-          artist_image_url?: string | null
-          artist_name?: string | null
-          artist_type_id?: number | null
-          spotify_desc?: string | null
-          spotify_url?: string | null
-        }
-        Update: {
-          artist_id?: number
-          artist_image_url?: string | null
-          artist_name?: string | null
-          artist_type_id?: number | null
-          spotify_desc?: string | null
-          spotify_url?: string | null
-        }
-        Relationships: [
-          {
-            foreignKeyName: "artists_artist_type_id_fkey"
-            columns: ["artist_type_id"]
-            isOneToOne: false
-            referencedRelation: "types"
-            referencedColumns: ["type_id"]
-          },
-        ]
-      }
-      genres: {
-        Row: {
-          genre_id: number
-          genre_name: string | null
-        }
-        Insert: {
-          genre_id: number
-          genre_name?: string | null
-        }
-        Update: {
-          genre_id?: number
-          genre_name?: string | null
-        }
-        Relationships: []
-      }
-      playlists: {
-        Row: {
-          id: number
-          playlist_id: number | null
-          song_id: number | null
-        }
-        Insert: {
-          id?: number
-          playlist_id?: number | null
-          song_id?: number | null
-        }
-        Update: {
-          id?: number
-          playlist_id?: number | null
-          song_id?: number | null
-        }
-        Relationships: [
-          {
-            foreignKeyName: "playlists_song_id_fkey"
-            columns: ["song_id"]
-            isOneToOne: false
-            referencedRelation: "songs"
-            referencedColumns: ["song_id"]
-          },
-        ]
-      }
-      songs: {
-        Row: {
-          acousticness: number | null
-          artist_id: number | null
-          bpm: number | null
-          danceability: number | null
-          duration: number | null
-          energy: number | null
-          genre_id: number | null
-          liveness: number | null
-          loudness: number | null
-          popularity: number | null
-          song_id: number
-          speechiness: number | null
-          title: string | null
-          valence: number | null
-          year: number | null
-        }
-        Insert: {
-          acousticness?: number | null
-          artist_id?: number | null
-          bpm?: number | null
-          danceability?: number | null
-          duration?: number | null
-          energy?: number | null
-          genre_id?: number | null
-          liveness?: number | null
-          loudness?: number | null
-          popularity?: number | null
-          song_id: number
-          speechiness?: number | null
-          title?: string | null
-          valence?: number | null
-          year?: number | null
-        }
-        Update: {
-          acousticness?: number | null
-          artist_id?: number | null
-          bpm?: number | null
-          danceability?: number | null
-          duration?: number | null
-          energy?: number | null
-          genre_id?: number | null
-          liveness?: number | null
-          loudness?: number | null
-          popularity?: number | null
-          song_id?: number
-          speechiness?: number | null
-          title?: string | null
-          valence?: number | null
-          year?: number | null
-        }
-        Relationships: [
-          {
-            foreignKeyName: "songs_artist_id_fkey"
-            columns: ["artist_id"]
-            isOneToOne: false
-            referencedRelation: "artists"
-            referencedColumns: ["artist_id"]
-          },
-          {
-            foreignKeyName: "songs_genre_id_fkey"
-            columns: ["genre_id"]
-            isOneToOne: false
-            referencedRelation: "genres"
-            referencedColumns: ["genre_id"]
-          },
-        ]
-      }
-      types: {
-        Row: {
-          type_id: number
-          type_name: string | null
-        }
-        Insert: {
-          type_id: number
-          type_name?: string | null
-        }
-        Update: {
-          type_id?: number
-          type_name?: string | null
-        }
-        Relationships: []
-      }
+    CompositeTypes: {
+      [_ in never]: never
     }
-    Views: {
+    Enums: {
       [_ in never]: never
     }
     Functions: {
       [_ in never]: never
     }
-    Enums: {
+    Tables: {
+      artists: {
+        Insert: {
+          artist_id: number
+          artist_image_url?: null | string
+          artist_name?: null | string
+          artist_type_id?: null | number
+          spotify_desc?: null | string
+          spotify_url?: null | string
+        }
+        Relationships: [
+          {
+            columns: ["artist_type_id"]
+            foreignKeyName: "artists_artist_type_id_fkey"
+            isOneToOne: false
+            referencedColumns: ["type_id"]
+            referencedRelation: "types"
+          },
+        ]
+        Row: {
+          artist_id: number
+          artist_image_url: null | string
+          artist_name: null | string
+          artist_type_id: null | number
+          spotify_desc: null | string
+          spotify_url: null | string
+        }
+        Update: {
+          artist_id?: number
+          artist_image_url?: null | string
+          artist_name?: null | string
+          artist_type_id?: null | number
+          spotify_desc?: null | string
+          spotify_url?: null | string
+        }
+      }
+      genres: {
+        Insert: {
+          genre_id: number
+          genre_name?: null | string
+        }
+        Relationships: []
+        Row: {
+          genre_id: number
+          genre_name: null | string
+        }
+        Update: {
+          genre_id?: number
+          genre_name?: null | string
+        }
+      }
+      playlists: {
+        Insert: {
+          id?: number
+          playlist_id?: null | number
+          song_id?: null | number
+        }
+        Relationships: [
+          {
+            columns: ["song_id"]
+            foreignKeyName: "playlists_song_id_fkey"
+            isOneToOne: false
+            referencedColumns: ["song_id"]
+            referencedRelation: "songs"
+          },
+        ]
+        Row: {
+          id: number
+          playlist_id: null | number
+          song_id: null | number
+        }
+        Update: {
+          id?: number
+          playlist_id?: null | number
+          song_id?: null | number
+        }
+      }
+      songs: {
+        Insert: {
+          acousticness?: null | number
+          artist_id?: null | number
+          bpm?: null | number
+          danceability?: null | number
+          duration?: null | number
+          energy?: null | number
+          genre_id?: null | number
+          liveness?: null | number
+          loudness?: null | number
+          popularity?: null | number
+          song_id: number
+          speechiness?: null | number
+          title?: null | string
+          valence?: null | number
+          year?: null | number
+        }
+        Relationships: [
+          {
+            columns: ["artist_id"]
+            foreignKeyName: "songs_artist_id_fkey"
+            isOneToOne: false
+            referencedColumns: ["artist_id"]
+            referencedRelation: "artists"
+          },
+          {
+            columns: ["genre_id"]
+            foreignKeyName: "songs_genre_id_fkey"
+            isOneToOne: false
+            referencedColumns: ["genre_id"]
+            referencedRelation: "genres"
+          },
+        ]
+        Row: {
+          acousticness: null | number
+          artist_id: null | number
+          bpm: null | number
+          danceability: null | number
+          duration: null | number
+          energy: null | number
+          genre_id: null | number
+          liveness: null | number
+          loudness: null | number
+          popularity: null | number
+          song_id: number
+          speechiness: null | number
+          title: null | string
+          valence: null | number
+          year: null | number
+        }
+        Update: {
+          acousticness?: null | number
+          artist_id?: null | number
+          bpm?: null | number
+          danceability?: null | number
+          duration?: null | number
+          energy?: null | number
+          genre_id?: null | number
+          liveness?: null | number
+          loudness?: null | number
+          popularity?: null | number
+          song_id?: number
+          speechiness?: null | number
+          title?: null | string
+          valence?: null | number
+          year?: null | number
+        }
+      }
+      types: {
+        Insert: {
+          type_id: number
+          type_name?: null | string
+        }
+        Relationships: []
+        Row: {
+          type_id: number
+          type_name: null | string
+        }
+        Update: {
+          type_id?: number
+          type_name?: null | string
+        }
+      }
+    }
+    Views: {
       [_ in never]: never
     }
-    CompositeTypes: {
-      [_ in never]: never
-    }
   }
-}
-
-type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">
-
-type DefaultSchema = DatabaseWithoutInternals[Extract<keyof Database, "public">]
-
-export type Tables<
-  DefaultSchemaTableNameOrOptions extends
-    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
-    | { schema: keyof DatabaseWithoutInternals },
-  TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals
-  }
-    ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-        DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
-    : never = never,
-> = DefaultSchemaTableNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals
-}
-  ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-      DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
-    }
-    ? R
-    : never
-  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
-        DefaultSchema["Views"])
-    ? (DefaultSchema["Tables"] &
-        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
-        Row: infer R
-      }
-      ? R
-      : never
-    : never
-
-export type TablesInsert<
-  DefaultSchemaTableNameOrOptions extends
-    | keyof DefaultSchema["Tables"]
-    | { schema: keyof DatabaseWithoutInternals },
-  TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals
-  }
-    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
-> = DefaultSchemaTableNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals
-}
-  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
-    }
-    ? I
-    : never
-  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
-    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Insert: infer I
-      }
-      ? I
-      : never
-    : never
-
-export type TablesUpdate<
-  DefaultSchemaTableNameOrOptions extends
-    | keyof DefaultSchema["Tables"]
-    | { schema: keyof DatabaseWithoutInternals },
-  TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals
-  }
-    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
-> = DefaultSchemaTableNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals
-}
-  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
-    }
-    ? U
-    : never
-  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
-    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Update: infer U
-      }
-      ? U
-      : never
-    : never
+};
 
 export type Enums<
   DefaultSchemaEnumNameOrOptions extends
@@ -308,24 +234,98 @@ export type Enums<
   ? DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
     ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
-    : never
+    : never;
 
-export type CompositeTypes<
-  PublicCompositeTypeNameOrOptions extends
-    | keyof DefaultSchema["CompositeTypes"]
+export type Json =
+  | boolean
+  | Json[]
+  | null
+  | number
+  | string
+  | { [key: string]: Json | undefined };
+
+export type Tables<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
     | { schema: keyof DatabaseWithoutInternals },
-  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+  TableName extends DefaultSchemaTableNameOrOptions extends {
     schema: keyof DatabaseWithoutInternals
   }
-    ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+        DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
     : never = never,
-> = PublicCompositeTypeNameOrOptions extends {
+> = DefaultSchemaTableNameOrOptions extends {
   schema: keyof DatabaseWithoutInternals
 }
-  ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
-  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
-    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+  ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+      DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+    Row: infer R
+  }
+    ? R
     : never
+  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])
+    ? (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
+      Row: infer R
+    }
+      ? R
+      : never
+    : never;
+
+export type TablesInsert<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
+    | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+    Insert: infer I
+  }
+    ? I
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+      Insert: infer I
+    }
+      ? I
+      : never
+    : never;
+
+export type TablesUpdate<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
+    | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+    Update: infer U
+  }
+    ? U
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+      Update: infer U
+    }
+      ? U
+      : never
+    : never;
+
+type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">;
+
+type DefaultSchema = DatabaseWithoutInternals[Extract<keyof Database, "public">];
 
 export const Constants = {
   graphql_public: {
@@ -334,5 +334,5 @@ export const Constants = {
   public: {
     Enums: {},
   },
-} as const
+} as const;
 
